02fce79144794223c43b4a3de56f21f3529adb23
Zach Ahn <zahn@handy.com>
2016-03-28 18:30:15 -0400

    mostly just swap bksp and enter

--- keyboard/ergodox_ez/keymaps/zachahn/keymap.c
+++ keyboard/ergodox_ez/keymaps/zachahn/keymap.c
@@ -15,10 +15,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  Tab   |   Q  |   W  |   E  |   R  |   T  | Hyper|           | Meh  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Ctrl/Esc|   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |  ;   |   '    |
- * |--------+------+------+------+------+------| Enter|           |Enter |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|  Del |           | BkSp |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |  /   | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |Grv/L1|  '"  |AltShf| Alt  | LCmd |                                       | ~L2  | RCmd |   [  |   ]  | ~L1  |
+ *   |  ~L1 |Ctrl/E|AltShf| Alt  | LCmd |                                       | ~L2  | RCmd |   [  |   ]  | ~L1  |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
@@ -35,20 +35,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,         KC_2,           KC_3,   KC_4,   KC_5,   KC_6,
         KC_TAB,         KC_Q,         KC_W,           KC_E,   KC_R,   KC_T,   ALL_T(KC_NO),
   CTL_T(KC_ESC),        KC_A,         KC_S,           KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,           KC_C,   KC_V,   KC_B,   KC_ENT,
-        LT(SYMB,KC_GRV),CTL_T(KC_ESC),LALT(KC_LSFT),  KC_LALT,KC_LGUI,
+        KC_LSFT,        KC_Z,         KC_X,           KC_C,   KC_V,   KC_B,   KC_DEL,
+        MO(SYMB),     CTL_T(KC_ESC),LALT(KC_LSFT),  KC_LALT,KC_LGUI,
                                               ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
-                                               KC_SPC,KC_DEL, KC_END,
+                                               KC_SPC,KC_ENT, KC_END,
         // right hand
              KC_BSPC,     KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,           KC_EQL,
              MEH_T(KC_NO),KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,    GUI_T(KC_QUOT),
-             KC_ENT,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
+             KC_BKSP,     KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          KC_RSFT,
                                MO(MDIA),  KC_RGUI,KC_LBRC,KC_RBRC,          MO(SYMB),
              KC_LALT,        CTL_T(KC_ESC),
              KC_PGUP,
-             KC_PGDN,KC_BSPC,KC_SPC
+             KC_PGDN,KC_ENT, KC_SPC
     ),
 /* Keymap 1: Symbol Layer
  *
